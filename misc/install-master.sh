8b014d09488c8af5a88965c581d33c74b546bdfd
kurtbradd <kurtbradd@gmail.com>
2016-05-18 18:44:43 -0400

    updated master init script

--- misc/install-master.sh
+++ misc/install-master.sh
@@ -1,37 +1,32 @@
-# Install Language Pack
-sudo apt-get install language-pack-en
+sudo apt-get update
 
-# Install Git
-sudo apt-get install git
+sudo apt-get upgrade
 
-# Install ZSH
-sudo apt-get install zsh
+# Install Required Packages
+sudo apt-get install language-pack-en git zsh htop make g++ libpq-dev postgresql postgresql-contrib
 
 # Install Oh-My-Zsh
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-#Install HTOP
-sudo apt-get install htop 
-
-
 
 # Install Redis
 sudo add-apt-repository ppa:chris-lea/redis-server
 sudo apt-get update
 sudo apt-get install redis-server
-
-# Install Postgres
-sudo apt-get install postgresql postgresql-contrib
+# Edit Redis conf to allow remote connections /etc/redis/redis.conf
 
 sudo -i -u 
 createdb crawler
-# create user crawl_worker with password 'password';
+# create database crawler;
+# create user crawl_worker with password 'mybadpassword';
 # GRANT ALL ON DATABASE crawler to crawl_worker;
+# Edit /var/lib/pgsql/data/pg_hba.conf to allow remote auth
+# Edit /etc/postgresql/9.1/main/postgresql.conf to allow remote connections
+# Reload postgresql service
 
 
 # Install NVM
-curl https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-source .bashrc
+curl https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | zsh
+source .zshrc
 
 # Install Node
 nvm install 4.2.6
