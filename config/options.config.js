39ff5970d9d94e7400b608d8977a7efdae2f2634
Dylan Aspden <dhaspden@gmail.com>
2017-04-22 17:49:27 -0400

    Refactor: validate existence of crawler and data

--- config/options.config.js
+++ config/options.config.js
@@ -2,6 +2,22 @@
 
 // Load dependencies
 const commandLineArguments = require('command-line-args');
+const fs = require('fs');
 
-const definitions = [{ name: 'data-file', type: String }];
-exports = module.exports = commandLineArguments(definitions);
+const definitions = [
+  { name: 'crawler', type: String },
+  { name: 'data-file', type: String }
+];
+const options = commandLineArguments(definitions);
+
+// Validate that the crawler file exists
+if (!fs.existsSync(options.crawler)) {
+  throw new Error('options: crawler not found');
+}
+
+// Validate that the data file exists
+if (!fs.existsSync(options['data-file'])) {
+  throw new Error('options: data file not found');
+}
+
+exports = module.exports = options;
