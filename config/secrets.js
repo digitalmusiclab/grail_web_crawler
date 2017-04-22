74699c768612e8a3d647777e8cb28b802a6ace46
Dylan Aspden <dhaspden@gmail.com>
2017-04-22 16:11:35 -0400

    Refactor: formatting

--- config/secrets.js
+++ config/secrets.js
@@ -1,36 +1,36 @@
 module.exports = {
-	development: {
-		database: {
-			redis: {
-				host: "0.0.0.0",
-				port: 6379,
-				db: 0,
-				options: {}
-			},
-			postgres: {
-				database: "crawler",
-				port: 5432,
-				username: "crawl_worker",
-				password: "mybadpassword",
-				opts: {
-					host: "0.0.0.0",
-					dialect: "postgres",
-					logging: false
-				}
-			}
-		},
-		queue: {
-			kue: {
-				prefix: "crawler_kue",
-				jobEvents: false,
-				disableSearch: true,
-				redis: {
-					host: "0.0.0.0",
-					port: 6379,
-					db: 0,
-					options: {}
-				}
-			}
-		} 
-	}
-}
+  development: {
+    database: {
+      redis: {
+        host: '0.0.0.0',
+        port: 6379,
+        db: 0,
+        options: {}
+      },
+      postgres: {
+        database: 'crawler',
+        port: 5432,
+        username: 'crawl_worker',
+        password: 'mybadpassword',
+        opts: {
+          host: '0.0.0.0',
+          dialect: 'postgres',
+          logging: false
+        }
+      }
+    },
+    queue: {
+      kue: {
+        prefix: 'crawler_kue',
+        jobEvents: false,
+        disableSearch: true,
+        redis: {
+          host: '0.0.0.0',
+          port: 6379,
+          db: 0,
+          options: {}
+        }
+      }
+    }
+  }
+};
