181eaa9d253029b7037f2ca34333dd7b2deaa8da
kurtbradd <kurtbradd@gmail.com>
2017-05-03 15:28:18 -0400

    Refactor: Job Processor Structure

---
+++ processors/musicbrainz/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+exports.releaseById = require('./release-by-id');
+exports.releaseByArtistAndName = require('./release-by-artist-and-name');
---
+++ processors/spotify/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.albumById = require('./albums-by-ids'),
+exports.albumsByIds = require('./album-by-id'),
+exports.tracksByIsrc = require('./tracks-by-isrc')
